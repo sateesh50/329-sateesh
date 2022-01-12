5524bcc2da8948b574e9af188ce40fbbdc137d94
sateesh50 <sateeshchalla52@gmail.com>
2022-01-12 14:22:08 +0530

    java assignment

---
+++ 329-sateesh-master/Java refresher assignment/ATM Applicaton.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+public class ATM {
+	public static void main(String args[]) {
+		System.out.print("Hello!\n");
+		int balance = 10000, withdraw, deposit;
+		long Account_Number;
+		long acc[] = { 31586, 36451 }; //Account numbers
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to ATM");  
+        	System.out.println("Choose 1 for Withdraw");  
+        	System.out.println("Choose 2 for Deposit");  
+        	System.out.println("Choose 3 for Check Balance");  
+        	System.out.println("Choose 4 for EXIT");  
+        	System.out.print("Choose the operation you want to perform:");  
+		while (true) {
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+			{
+			    System.out.print("Enter Account Number:");
+				Account_Number = sc.nextLong();
+				if(Account_Number==acc[0] || Account_Number==acc[1])
+				{
+				System.out.print("Enter money to be withdrawn: ");
+				withdraw = sc.nextInt();
+				    if (balance >= withdraw) {
+				    	balance = balance - withdraw;
+					    System.out.println("Please collect your money\n");
+					    System.out.println("Your remaining balance is: "+balance);
+					} 
+				    else {
+					    System.out.println("Insufficient Balance");
+				    }
+				System.out.println("");
+				}
+				else
+				{
+					System.out.println("Enter correct account number ");
+				}
+				break;
+			}
+			case 2:
+			{
+				System.out.print("Enter Account Number:");
+				Account_Number = sc.nextLong();
+				if(Account_Number==acc[0] || Account_Number==acc[1])
+				{
+				System.out.print("Enter money to be deposited:");
+				deposit = sc.nextInt();
+				balance = balance + deposit;
+				System.out.println("Your Money has been successfully deposited! \n");
+				System.out.println("Your balance is: "+balance);
+				}
+				else
+				{
+					System.out.println("Enter correct account number");
+				}
+				break;
+			}
+			case 3:  
+            {
+                System.out.println("Your balance is: "+balance);  
+                System.out.println("");  
+                break;  
+            }
+            case 4:  
+            {
+                System.exit(0);  
+            }
+			default:
+			{
+			    System.out.println("Wrong option");
+			}
+			}
+		}
+	}
+}
